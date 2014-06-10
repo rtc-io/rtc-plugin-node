369514399ff0cce915080167a9fbd49f5887aa7c
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-10 17:41:51 +1000

    Completed implementation

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var webrtc = require('wrtc');
 
+/**
+  # rtc-plugin-node
+
+
+
+**/
+exports.supported = function(platform) {
+  return platform && platform.browser === 'node';
+};
+
+exports.init = function(opts, callback) {
+  callback();
+};
+
 exports.createIceCandidate = function(opts) {
+  return new webrtc.RTCIceCandidate(opts);
 };
 
 exports.createConnection = function(config, constraints) {
+  return new webrtc.RTCPeerConnection(config, constraints);
 };
 
 exports.createSessionDescription = function(opts) {
+  return new webrtc.RTCSessionDescription(opts);
 };
