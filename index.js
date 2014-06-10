7cfe9b5b368488cd6dcd5abf48cea7b7ab8f9e8e
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-10 14:29:27 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var webrtc = require('wrtc');
+
+exports.createIceCandidate = function(opts) {
+};
+
+exports.createConnection = function(config, constraints) {
+};
+
+exports.createSessionDescription = function(opts) {
+};
