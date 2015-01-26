d1a2079788bf3d2ebda7ece34cb63a2c5ce2c422
Damon Oehlman <damon.oehlman@gmail.com>
2015-01-27 08:35:15 +1100

    Refactor tests to start server as part of tests

--- test/all.js
+++ test/all.js
@@ -1,1 +1,16 @@
+var test = require('tape');
+var server = require('./server');
+var port = process.env.ZUUL_PORT || process.env.PORT;
+
+test('start local signalling server', function(t) {
+  t.plan(1);
+  server.listen(parseInt(port, 10) || 3000, t.ifError);
+});
+
 require('./dc-connect');
+
+test('close server', function(t) {
+  t.plan(1);
+  server.close();
+  t.pass('server closed');
+});
--- test/server.js
+++ test/server.js
@@ -1,6 +1,3 @@
 var http = require('http');
-var server = http.createServer();
-var port = process.env.ZUUL_PORT || process.env.PORT;
+var server = module.exports = http.createServer();
 var switchboard = require('rtc-switchboard')(server, { servelib: true });
-
-server.listen(parseInt(port, 10) || 3000);
